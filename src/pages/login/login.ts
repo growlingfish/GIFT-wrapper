2d9269587bf0aecfa500b2ab73049e6e059b535e
Ben Bedwell <github@growlingfish.com>
2017-07-10 13:23:26 +0100

    Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { HomePage } from '../home/home';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-login',
@@ -11,7 +12,12 @@ export class LoginPage {
   loading: Loading;
   registerCredentials = { email: '', password: '' };
 
-  constructor(private nav: NavController, private auth: AuthServiceProvider, private alertCtrl: AlertController, private loadingCtrl: LoadingController) {
+  constructor(private nav: NavController, private auth: AuthServiceProvider, private alertCtrl: AlertController, private loadingCtrl: LoadingController, public storage: Storage) {
+    storage.get('loginEmail').then((loginEmail) => {
+      this.registerCredentials.email = loginEmail;
+    }).catch(
+      console.log.bind(console)
+    );
   }
 
   public createAccount() {
@@ -19,7 +25,8 @@ export class LoginPage {
   }
 
   public login() {
-    this.showLoading()
+    this.showLoading();
+    this.storage.set('loginEmail', this.registerCredentials.email);
     this.auth.login(this.registerCredentials).subscribe(allowed => {
       if (allowed) {
         this.nav.setRoot(HomePage);
